e0e5d67b694d3fccb424493c13aa98bdc5d3f7e3
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-06-05 22:56:49 -0500

    feat: :sparkles: create partials and add features to map such as bounds and markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -10,19 +11,36 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12' // style URL
+      style: 'mapbox://styles/segundorebaza/clx2q37p202d301qp6vbu2cx8' // style URL
       // center: [-74.5, 40], // starting position [lng, lat]
       // zoom: 9, // starting zoom
     });
     this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                             mapboxgl: mapboxgl }));
   }
 
   #addMarkersToMap () {
     // Create a new marker.
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+      const customMarker = document.createElement('div');
+      customMarker.innerHTML = marker.marker_html;
+
+      new mapboxgl.Marker(customMarker)
                   .setLngLat([marker.lng, marker.lat])
+                  .setPopup(popup)
                   .addTo(this.map);
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    this.map.fitBounds(bounds, {padding: 70, maxZoom: 15, duration: 0});
   }
 }
