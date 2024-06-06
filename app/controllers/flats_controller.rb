1f6abbd1f34dd7e0754b1bf588150a034d38ad57
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-06-05 22:56:24 -0500

    feat: :sparkles: pass partials to stimulus inside controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,7 +8,9 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { flat: flat }),
+        marker_html: render_to_string(partial: 'marker', locals: { flat: flat })
       }
     end
   end
